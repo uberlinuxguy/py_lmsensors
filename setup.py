23a5a98e2aec5e8c27489b69312f215c0491116d
Jason Williams <jasonw@jhu.edu>
2022-02-15 16:42:37 -0500

    Add setup.py for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from py_lmsensors import __version__
+
+setup(
+  name = "py_lmsensors",
+  version=__version__,
+  url='https://github.com/uberlinuxguy/py_lmsensors',
+  author="Jason Williams",
+  author_email='uberlinuxguy@gmail.com',
+  py_modules=['py_lmsensors'],
+)
